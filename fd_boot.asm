1a22105f5fbc9ed3bf6e457275add91995754ee4
pc98user <pc98user@pc98user-HVM-domU>
2023-03-18 21:02:42 +0900

    ドライブを選択可能にする

--- fd_boot.asm
+++ fd_boot.asm
@@ -1,34 +1,78 @@
 ;===========================================================
 ; fd_boot.asm
 ;
-; FD#3(DA/UA=92h)からブートする
+; FDからブートする(ドライブ選択可)
 ;===========================================================
 cpu		8086
 bits		16
 
-DAUA		equ	92h
-SECSIZE		equ	1024
+DAUA		equ	90h
+SECSIZE		equ	3	; 0=128Byte, 1=256Byte, 2=512Byte, 3=1024Byte
+READLEN		equ	1024
 LOADSEG		equ	1fc0h
 
 org		0100h
 start:
+daua_input:
+		push	cs
+		pop	ds
+		mov	si, msg_input
+		call	puts
+
 		xor	ax, ax
 		mov	ds, ax
-		mov	ax, LOADSEG
-		mov	es, ax
 
-		mov	ah, 0d6h
-		mov	al, DAUA
+		mov	ah, 00h		; キー入力
+		int	18h
+
+		cmp	al, '1'
+		jb	daua_input
+		cmp	al, '4'
+		ja	daua_input
+
+		sub	al, '1'
+		add	al, DAUA
 		mov	[0584h], al
-		mov	bx, SECSIZE
-		mov	ch, 3		; n=1024
+
+		mov	bx, READLEN
+		mov	ch, SECSIZE
 		mov	cl, 0		; c=0
 		mov	dh, 0		; h=0
 		mov	dl, 1		; s=1
-
+		mov	ax, LOADSEG
+		mov	es, ax
 		mov	bp, 0000h
-		mov	si, bp
-
+		mov	al, [0584h]
+		mov	ah, 0d6h
 		int	1bh
 
+		jnb	boot		; CF=0(no error)
+
+		push	cs
+		pop	ds
+		mov	si, msg_error
+		call	puts
+		mov	al, ah
+		call	preg8
+		mov	al, 0ah
+		call	putc
+		jmp	daua_input
+
+boot:
+		push	cs
+		pop	ds
+		mov	si, msg_booting
+		call	puts
+
+		xor	ax, ax
+		mov	ds, ax
+
 		jmp	LOADSEG:0000h
+
+
+msg_input	db	"-- Input FD drive number (1-4) --", 0ah, 0
+msg_error	db	"Read error! AH=", 0
+msg_booting	db	"Booting...", 0ah, 0
+
+%include "printlib.inc"
+%include "printreg.inc"
